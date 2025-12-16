1cf346af0646f4f95ddc344f2826b9846548a1a8
Alden Aik Tai Yuan <88491193+buttsaggs@users.noreply.github.com>
2025-12-16 22:33:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from boundary.landing_ui import landing_bp
+from boundary.user_ui import user_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "change-this-secret-key"
 app.register_blueprint(landing_bp)
+app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
