fb2b8c95b994a1b91280d8066f18f34212674a30
GTMGRADE <sazabigrade@gmail.com>
2025-12-17 03:19:41 +0800

    Update landing page and add reviews feature

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from boundary.landing_ui import landing_bp
+from boundary.reviews_ui import reviews_bp
 
 app = Flask(__name__)
 app.register_blueprint(landing_bp)
+app.register_blueprint(reviews_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
